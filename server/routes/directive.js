472964b972ec23eb9cbe4709a834ad6534766570
cong <616831469@qq.com>
2019-03-24 15:39:21 +0800

    添加注释

--- server/routes/directive.js
+++ server/routes/directive.js
@@ -4,6 +4,7 @@ const express = require('express');
 var router = express.Router();
 
 router.post('/stock', function (req, res) {
+    // 返回前端Json
     res.json({
         code: 0,
         data: [{
